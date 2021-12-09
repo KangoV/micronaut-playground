3d9004965d4279d3247a0588fe0952aa13f17657
darren <djb.bell@gmail.com>
2021-12-09 23:47:50 +0000

    events-backend receiving kafka messages correctly

--- modules/events-backend/src/main/java/io/belldj/pg/events/EventsListener.java
+++ modules/events-backend/src/main/java/io/belldj/pg/events/EventsListener.java
@@ -1,23 +1,16 @@
 package io.belldj.pg.events;
 
-import io.micronaut.configuration.kafka.annotation.KafkaListener;
-import io.micronaut.configuration.kafka.annotation.Topic;
-import io.micronaut.context.annotation.Requires;
-import io.micronaut.context.env.Environment;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.micronaut.configuration.kafka.annotation.*;
+import org.slf4j.*;
 
-import java.awt.print.Book;
-
-@Requires(notEnv = Environment.TEST) 
-@KafkaListener 
+@KafkaListener
 public class EventsListener {
 
     private static final Logger log = LoggerFactory.getLogger(EventsListener.class);
 
     @Topic("events")
-    public void notify(String string) {
-         log.info("got: "+string);
+    void receive(@KafkaKey String key, String event) {
+      System.out.println("### Received from kafka: "+event);
     }
 
 }
